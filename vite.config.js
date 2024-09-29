3c8e6d1d34cde50507f68de726b073fa57d604ab
RosieOh <20172207@gm.hannam.ac.kr>
2024-09-30 00:53:15 +0900

    ⚡️ FEAT : Vuetify 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
