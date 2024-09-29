8eab01ff6352aee88afe6a59c3da43b213b1c9fd
RosieOh <20172207@gm.hannam.ac.kr>
2024-09-30 00:54:17 +0900

    ✨FEAT : 테스트 페이지 작성

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" app> 탭 안에 있을 UI </v-navigation-drawer>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Test Application</v-toolbar-title>
+    </v-app-bar>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <v-main>
+      <v-card class="mx-auto" max-width="344">
+        <v-card-text>
+          <div>기본 UI 설정</div>
+          <p class="display-1 text--primary">responsive web design</p>
+          <div class="text--primary">TEST Page</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="deep-purple accent-4"> Learn More </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer: null
+    }
   }
 }
-</style>
+</script>
